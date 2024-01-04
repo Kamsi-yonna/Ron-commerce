b0f02f0a3628cc727bb4421bf45fa07bcd08e2ba
Kamsi-yonna <etiobikamsi@gmail.com>
2024-01-04 16:33:07 +0100

    feat - - checkout page setup

---
+++ pages/checkout.vue
@@ -0,0 +1,200 @@
+<template>
+  <NuxtLayout>
+    <div
+      id="CheckoutPage"
+      class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-beween mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-xl font-semibold mb-2">Shipping Address</div>
+
+            <div v-if="true">
+              <NuxtLink
+                class="flex iitems-center pv-2 text-blue-500 hover:text-red-400"
+                to="/address">
+                <Icon
+                  class="mr-2"
+                  name="mdi:plus"
+                  size="18" />
+                Update Address
+              </NuxtLink>
+
+              <div class="pt-2 border-t">
+                <div class="underline pb-1">Delivery Address</div>
+                <ul class="text-xs">
+                  <li class="flex items-center gap-2 mt-2">
+                    <div>Contact name:</div>
+                    <div class="font-bold">Current Address name</div>
+                  </li>
+
+                  <li class="flex items-center gap-2 mt-2">
+                    <div>Address:</div>
+                    <div class="font-bold">Current Address</div>
+                  </li>
+
+                  <li class="flex items-center gap-2 mt-2">
+                    <div>Zip Code:</div>
+                    <div class="font-bold">Current Address Zipcode</div>
+                  </li>
+
+                  <li class="flex items-center gap-2 mt-2">
+                    <div>City:</div>
+                    <div class="font-bold">Current Address City</div>
+                  </li>
+
+                  <li class="flex items-center gap-2 mt-2">
+                    <div>Country:</div>
+                    <div class="font-bold">Current Address country</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <NuxtLink
+              v-else
+              class="flex items-center text-blue-500 hover:text-red-400"
+              to="/address">
+              <Icon
+                class="mr-2"
+                name="mdi:plus"
+                size="18"></Icon>
+            </NuxtLink>
+          </div>
+
+          <div
+            id="Items"
+            class="bg-white rounded-lg p-4 mt-4">
+            <div
+              v-for="(product, index) in products"
+              :key="index">
+              <CheckoutItem :product="product" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+// import { useUserStore } from '~/composables/useUserStore'
+// const userStore = useUserStore()
+// const route = useRoute()
+
+// const stripe = null
+// const elements = null
+// const card = null
+// const form = null
+// const total = ref(0)
+// const clientSecret = null
+// const currentAddress = ref(null)
+// const isProcessing = ref(false)
+
+interface Product {
+  id: number
+  title: string
+  description: string
+  url: string
+  price: number
+}
+
+// onMounted(() => {
+//   isProcessing.value = true
+
+//   useUserStore.checkout.forEach((item) => {
+//     total.value += item.price
+//   })
+// })
+
+// watch(
+//   () => total.value,
+//   () => {
+//     if (totalmem.value > 0) {
+//       StripeInit()
+//     }
+//   }
+// )
+
+// const stripeInit = async () => {}
+
+// const pay = async () => {}
+
+// const createOrder = async (stripeId) => {}
+
+// const showError = async (errorMsgTxt) => {}
+
+const products: Product[] = [
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a good product',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 9000
+  },
+  {
+    id: 2,
+    title: 'Title 2',
+    description: 'This is another product',
+    url: 'https://picsum.photos/id/42/800/800',
+    price: 8500
+  },
+  {
+    id: 3,
+    title: 'Title 3',
+    description: 'Yet another product description',
+    url: 'https://picsum.photos/id/12/800/800',
+    price: 9500
+  },
+  {
+    id: 4,
+    title: 'Title 4',
+    description: 'Description for the fourth product',
+    url: 'https://picsum.photos/id/46/800/800',
+    price: 8000
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    description: 'Fifth product description',
+    url: 'https://picsum.photos/id/81/800/800',
+    price: 9200
+  },
+  {
+    id: 6,
+    title: 'Title 6',
+    description: 'Description for the sixth product',
+    url: 'https://picsum.photos/id/21/800/800',
+    price: 8700
+  },
+  {
+    id: 7,
+    title: 'Title 7',
+    description: 'Seventh product description',
+    url: 'https://picsum.photos/id/35/800/800',
+    price: 9100
+  },
+  {
+    id: 8,
+    title: 'Title 8',
+    description: 'Description for the eighth product',
+    url: 'https://picsum.photos/id/61/800/800',
+    price: 8300
+  },
+  {
+    id: 9,
+    title: 'Title 9',
+    description: 'Ninth product description',
+    url: 'https://picsum.photos/id/10/800/800',
+    price: 8900
+  },
+  {
+    id: 10,
+    title: 'Title 10',
+    description: 'This is the tenth product',
+    url: 'https://picsum.photos/id/78/800/800',
+    price: 8000
+  }
+]
+</script>
+
+<style scoped></style>
