7f810f9b45ee5d3989fe25d8ddba86140ae618df
Kamsi-yonna <etiobikamsi@gmail.com>
2024-01-04 20:48:29 +0100

    feat - checkoutItem component

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -178,62 +178,6 @@ const products: Product[] = [
     description: 'This is another product',
     url: 'https://picsum.photos/id/42/800/800',
     price: 8500
-  },
-  {
-    id: 3,
-    title: 'Title 3',
-    description: 'Yet another product description',
-    url: 'https://picsum.photos/id/12/800/800',
-    price: 9500
-  },
-  {
-    id: 4,
-    title: 'Title 4',
-    description: 'Description for the fourth product',
-    url: 'https://picsum.photos/id/46/800/800',
-    price: 8000
-  },
-  {
-    id: 5,
-    title: 'Title 5',
-    description: 'Fifth product description',
-    url: 'https://picsum.photos/id/81/800/800',
-    price: 9200
-  },
-  {
-    id: 6,
-    title: 'Title 6',
-    description: 'Description for the sixth product',
-    url: 'https://picsum.photos/id/21/800/800',
-    price: 8700
-  },
-  {
-    id: 7,
-    title: 'Title 7',
-    description: 'Seventh product description',
-    url: 'https://picsum.photos/id/35/800/800',
-    price: 9100
-  },
-  {
-    id: 8,
-    title: 'Title 8',
-    description: 'Description for the eighth product',
-    url: 'https://picsum.photos/id/61/800/800',
-    price: 8300
-  },
-  {
-    id: 9,
-    title: 'Title 9',
-    description: 'Ninth product description',
-    url: 'https://picsum.photos/id/10/800/800',
-    price: 8900
-  },
-  {
-    id: 10,
-    title: 'Title 10',
-    description: 'This is the tenth product',
-    url: 'https://picsum.photos/id/78/800/800',
-    price: 8000
   }
 ]
 </script>
