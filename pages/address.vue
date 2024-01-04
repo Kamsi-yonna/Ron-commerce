07994ae472bd85d29cc3ae05cf5d6e2fe638c4e3
Kamsi-yonna <etiobikamsi@gmail.com>
2024-01-04 21:43:01 +0100

    fix - address page ts errors

--- pages/address.vue
+++ pages/address.vue
@@ -5,34 +5,39 @@
       class="mt-4 max-w-[500px] mx-auto px-2">
       <div class="mx-auto bg-white rounded-lg p-3">
         <div class="text-xl text-bold mb-2">Address Details</div>
-        <form @submit.prevent="">
+        <form @submit.prevent="submit()">
           <TextInput
             v-model:input="contactName"
             class="w-full"
+            :error="error && error.type == 'contactName' ? error.message : ''"
             placeholder="Contact Name"
             type="text" />
 
           <TextInput
             v-model:input="address"
             class="w-full mt-2"
+            :error="error && error.type == 'address' ? error.message : ''"
             placeholder="Address"
             type="text" />
 
           <TextInput
             v-model:input="zipCode"
             class="w-full"
+            :error="error && error.type == 'zipCode' ? error.message : ''"
             placeholder="Zip Code"
             type="text" />
 
           <TextInput
             v-model:input="city"
             class="w-full"
+            :error="error && error.type == 'city' ? error.message : ''"
             placeholder="City"
             type="text" />
 
           <TextInput
             v-model:input="country"
             class="w-full"
+            :error="error && error.type == 'country' ? error.message : ''"
             placeholder="Country"
             type="text" />
 
@@ -52,8 +57,8 @@
 </template>
 
 <script setup lang="ts">
-// import { useUserStore } from '~/composables/useUserStore'
-// const userStore = useUserStore()
+import { useUserStore } from '~/composables/useUserStore'
+const userStore = useUserStore()
 
 const contactName = ref(null)
 const address = ref(null)
@@ -63,8 +68,48 @@ const country = ref(null)
 
 // const currentAddress = ref(null)
 // const isUpdate = ref(false)
-// const isWorking = ref(false)
-// const error = ref(null)
+const isWorking = ref(false)
+const error = ref<{ type: string; message: string } | null>(null)
+
+watchEffect(() => {
+  userStore.isLoading = false
+})
+
+const submit = () => {
+  isWorking.value = true
+  error.value = null
+
+  if (!contactName.value) {
+    error.value = {
+      type: 'contactName',
+      message: 'A contact name is required'
+    }
+  } else if (!address.value) {
+    error.value = {
+      type: 'address',
+      message: 'An address is required'
+    }
+  } else if (!zipCode.value) {
+    error.value = {
+      type: 'zipCode',
+      message: 'A zip code is required'
+    }
+  } else if (!city.value) {
+    error.value = {
+      type: 'city',
+      message: 'A city is required'
+    }
+  } else if (!country.value) {
+    error.value = {
+      type: 'country',
+      message: 'A country is required'
+    }
+  }
+
+  if (error.value) {
+    isWorking.value = false
+  }
+}
 </script>
 
 <style scoped></style>
